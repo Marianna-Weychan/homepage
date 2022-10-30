23926c9b564ee8465ba8c2aaef69bdc8bc809448
Marianna Weychan <marianna.weychan@wp.pl>
2022-10-30 11:16:03 +0100

    Added functions to java script

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,31 @@
-console.log("Witam serdecznie na mojej stronie :) !");
+{
+    const welcome = () => {
+        console.log("Witam serdecznie na mojej stronie :) !");
+    }
 
-let button = document.querySelector(".header__button");
-let header__title = document.querySelector(".header__title");
-let jsThemeName = document.querySelector(".jsThemeName");
+    const toggleColorHeader = () => {
+        const header__title = document.querySelector(".header__title");
+        const jsThemeName = document.querySelector(".jsThemeName");
+        header__title.classList.toggle("header__afterButton");
+
+        jsThemeName.innerText = header__title.classList.contains("header__afterButton") ? "Przywróć" : "Zmień"
+
+    };
+
+    const init = () => {
+        const button = document.querySelector(".header__button");
+        button.addEventListener("click", toggleColorHeader);
 
 
-button.addEventListener("click", () => {
+        welcome()
+    }
 
-    header__title.classList.toggle("header__afterButton");
+    init()
+};
 
-    jsThemeName.innerText = header__title.classList.contains("header__afterButton") ? "Przywróć" : "Zmień"
 
-}
-);
+
+
+
+
 
